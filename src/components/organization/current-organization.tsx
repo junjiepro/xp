02f620381776c8703ede1588c74c923ef16c245e
Junjie <2721795866@qq.com>
2024-04-03 23:59:44 +0800

    ✨ feat(component):
    update current-organization struct

--- src/components/organization/current-organization.tsx
+++ src/components/organization/current-organization.tsx
@@ -1,30 +1,60 @@
 "use client"
 
-import { useOrganizations } from "@/hooks/use-organizations";
+import * as React from "react";
+import { useOrganizations, useRoles } from "@/hooks/use-organizations";
 import { useTranslation } from "next-export-i18n";
 import { useSearchParams } from "next/navigation";
-import * as React from "react";
-
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 export default function CurrentOrganization() {
   const { t } = useTranslation();
+  const roles = useRoles();
   const organizations = useOrganizations();
   const searchParams = useSearchParams();
-  const selectedOrganization = React.useMemo(() => {
+  const currentOrganization = React.useMemo(() => {
     const organizationId = searchParams.get("organizationId");
     if (organizations && organizationId) {
       return organizations.find((o) => o.id === organizationId);
     }
     return undefined;
   }, [organizations, searchParams])
+  const currentRoles = React.useMemo(() => {
+    const organizationId = searchParams.get("organizationId");
+    if (roles && organizationId) {
+      return roles.filter((r) => r.organization_id === organizationId);
+    }
+    return [];
+  }, [roles, searchParams])
   return (
     <>{
-      selectedOrganization ? (
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
-            <h1 className="text-3xl font-bold">{selectedOrganization.name}</h1>
-          </div>
-        </div>
+      currentOrganization ? (
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="w-screen rounded-lg border"
+        >
+          <ResizablePanel defaultSize={25} minSize={10}>
+            <div className="h-[calc((100vh-68px))] flex flex-col">
+              <ScrollArea className="flex-auto w-full p-4">
+                <span className="font-semibold">Applications</span>
+              </ScrollArea>
+              <ScrollArea className="h-64 w-full border-t p-4">
+                <span className="font-semibold">Settings</span>
+              </ScrollArea>
+
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={75} minSize={10}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Content</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
       ) : null
     }</>
   )
