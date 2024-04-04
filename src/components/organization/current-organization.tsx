6b325c44262a13d1765bc58461987ba9c851233c
Junjie <2721795866@qq.com>
2024-04-04 23:20:25 +0800

    ✨ feat:

--- src/components/organization/current-organization.tsx
+++ src/components/organization/current-organization.tsx
@@ -10,6 +10,23 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable"
 import { ScrollArea } from "@/components/ui/scroll-area"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
+import {
+  Settings, Calculator,
+  Calendar,
+  CreditCard,
+  Smile,
+  User,
+} from "lucide-react";
 
 export default function CurrentOrganization({
   children,
@@ -43,13 +60,51 @@ export default function CurrentOrganization({
         >
           <ResizablePanel defaultSize={20} minSize={10}>
             <div className="h-[calc((100vh-68px))] flex flex-col">
-              <ScrollArea className="flex-auto w-full p-4">
-                <span className="font-semibold">Applications</span>
+              <ScrollArea className="flex-auto w-full p-0">
+                <Command>
+                  <CommandInput placeholder="Type a command or search..." />
+                  <CommandList>
+                    <CommandEmpty>No results found.</CommandEmpty>
+                    <CommandGroup heading="Suggestions">
+                      <CommandItem>
+                        <Calendar className="mr-2 h-4 w-4" />
+                        <span>Calendar</span>
+                      </CommandItem>
+                      <CommandItem>
+                        <Smile className="mr-2 h-4 w-4" />
+                        <span>Search Emoji</span>
+                      </CommandItem>
+                      <CommandItem>
+                        <Calculator className="mr-2 h-4 w-4" />
+                        <span>Calculator</span>
+                      </CommandItem>
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
               </ScrollArea>
-              <ScrollArea className="h-64 w-full border-t p-4">
-                <span className="font-semibold">Settings</span>
+              <ScrollArea className="h-64 w-full border-t p-0">
+                <Command>
+                  <CommandList>
+                    <CommandGroup heading="Settings">
+                      <CommandItem>
+                        <User className="mr-2 h-4 w-4" />
+                        <span>Profile</span>
+                        <CommandShortcut>⌘P</CommandShortcut>
+                      </CommandItem>
+                      <CommandItem>
+                        <CreditCard className="mr-2 h-4 w-4" />
+                        <span>Billing</span>
+                        <CommandShortcut>⌘B</CommandShortcut>
+                      </CommandItem>
+                      <CommandItem>
+                        <Settings className="mr-2 h-4 w-4" />
+                        <span>Settings</span>
+                        <CommandShortcut>⌘S</CommandShortcut>
+                      </CommandItem>
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
               </ScrollArea>
-
             </div>
           </ResizablePanel>
           <ResizableHandle withHandle />
