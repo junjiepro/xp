dbc59d4dd73a7afdde32217d3894500a135f3a13
Junjie <2721795866@qq.com>
2024-04-23 22:53:24 +0800

    remove use-supabase

--- src/hooks/use-supabase.ts
+++
@@ -1,9 +0,0 @@
-import { Database } from '@/types/database.types'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { atom, useAtomValue } from 'jotai'
-
-const supabase = atom(createClientComponentClient<Database>());
-
-export const useSupabase = () => {
-  return useAtomValue(supabase)
-}
