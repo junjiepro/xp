84ef004b580cdea126317b0a03db21ff881e26af
Junjie <2721795866@qq.com>
2024-06-26 19:25:19 +0800

    update organizayion layout, just collapsed

--- src/components/organization/current-organization.tsx
+++ src/components/organization/current-organization.tsx
@@ -1,33 +1,32 @@
-"use client"
+"use client";
 
 import * as React from "react";
-import { useOrganizationLayout, useOrganizations, useRoles, useSetOrganizationLayout } from "@/hooks/use-organizations";
+import {
+  useOrganizationLayout,
+  useOrganizations,
+  useRoles,
+  useSetOrganizationLayout,
+} from "@/hooks/use-organizations";
 import { useTranslation } from "next-export-i18n";
 import { useSearchParams } from "next/navigation";
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/components/ui/resizable"
-import { ScrollArea } from "@/components/ui/scroll-area"
-import {
-  Settings,
-  Gem,
-  User,
-  Bot,
-  Monitor,
-  Atom,
-} from "lucide-react";
+} from "@/components/ui/resizable";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Settings, Gem, User, Bot, Monitor, Atom } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Nav } from "@/components/nav";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Separator } from "../ui/separator";
 
 export default function CurrentOrganization({
   navCollapsedSize,
   children,
 }: {
-  navCollapsedSize: number
-  children: React.ReactNode
+  navCollapsedSize: number;
+  children: React.ReactNode;
 }) {
   const { t } = useTranslation();
   const roles = useRoles();
@@ -42,19 +41,19 @@ export default function CurrentOrganization({
       return organizations.find((o) => o.id === organizationId);
     }
     return undefined;
-  }, [organizations, searchParams])
+  }, [organizations, searchParams]);
   const currentRoles = React.useMemo(() => {
     const organizationId = searchParams.get("organizationId");
     if (roles && organizationId) {
       return roles.filter((r) => r.organization_id === organizationId);
     }
     return [];
-  }, [roles, searchParams])
+  }, [roles, searchParams]);
   const isAdmin = currentRoles.some((r) => r.role_name === "Administrator");
 
   return (
-    <>{
-      currentOrganization ? (
+    <>
+      {currentOrganization ? (
         <TooltipProvider delayDuration={0}>
           <ResizablePanelGroup
             direction="horizontal"
@@ -62,103 +61,140 @@ export default function CurrentOrganization({
               setOrganizationLayout({
                 ...organizationLayout,
                 layout: sizes,
-              })
+              });
             }}
             className="h-full max-h-[100vh] items-stretch"
           >
             <ResizablePanel
-              defaultSize={organizationLayout.layout[0]}
+              defaultSize={navCollapsedSize}
               collapsedSize={navCollapsedSize}
-              collapsible={true}
-              minSize={15}
-              maxSize={20}
+              // collapsible={true}
+              minSize={navCollapsedSize}
+              maxSize={navCollapsedSize}
               onCollapse={() => {
                 setOrganizationLayout({
                   ...organizationLayout,
                   collapsed: true,
-                })
+                });
               }}
               onExpand={() => {
                 setOrganizationLayout({
                   ...organizationLayout,
                   collapsed: false,
-                })
+                });
               }}
-              className={cn(organizationLayout.collapsed && "min-w-[50px] transition-all duration-300 ease-in-out")}
+              className={cn(
+                organizationLayout.collapsed &&
+                  "min-w-[50px] transition-all duration-300 ease-in-out"
+              )}
             >
               <div className="h-[calc((100vh-68px))] flex flex-col">
                 <ScrollArea className="flex-auto w-full p-0">
                   <Nav
                     isCollapsed={organizationLayout.collapsed}
-                    links={[{
-                      title: "工作空间",
-                      label: "",
-                      icon: Atom,
-                      variant: "ghost",
-                      path: `/organization/workspace`,
-                      param: `organizationId=${searchParams.get("organizationId")}`,
-                    }, {
-                      title: "个人空间",
-                      label: "",
-                      icon: User,
-                      variant: "ghost",
-                      path: `/organization/self`,
-                      param: `organizationId=${searchParams.get("organizationId")}`,
-                    }, {
-                      title: "XP LLM",
-                      label: "",
-                      icon: Bot,
-                      variant: "ghost",
-                      path: `/organization/xpllm`,
-                      param: `organizationId=${searchParams.get("organizationId")}`,
-                    }, {
-                      title: "演练场",
-                      label: "",
-                      icon: Monitor,
-                      variant: "ghost",
-                      path: `/organization/playground`,
-                      param: `organizationId=${searchParams.get("organizationId")}`,
-                    }]}
+                    links={[
+                      {
+                        title: "工作空间",
+                        label: "",
+                        icon: Atom,
+                        variant: "ghost",
+                        path: `/organization/workspace`,
+                        param: `organizationId=${searchParams.get(
+                          "organizationId"
+                        )}`,
+                      },
+                      {
+                        title: "个人空间",
+                        label: "",
+                        icon: User,
+                        variant: "ghost",
+                        path: `/organization/self`,
+                        param: `organizationId=${searchParams.get(
+                          "organizationId"
+                        )}`,
+                      },
+                      {
+                        title: "XP LLM",
+                        label: "",
+                        icon: Bot,
+                        variant: "ghost",
+                        path: `/organization/xpllm`,
+                        param: `organizationId=${searchParams.get(
+                          "organizationId"
+                        )}`,
+                      },
+                      {
+                        title: "演练场",
+                        label: "",
+                        icon: Monitor,
+                        variant: "ghost",
+                        path: `/organization/playground`,
+                        param: `organizationId=${searchParams.get(
+                          "organizationId"
+                        )}`,
+                      },
+                    ]}
                   />
                 </ScrollArea>
                 <ScrollArea className="h-64 w-full border-t p-0">
                   <Nav
                     isCollapsed={organizationLayout.collapsed}
-                    links={isAdmin ? [
-                      {
-                        title: t('common.profile'),
-                        label: "",
-                        icon: Gem,
-                        variant: "ghost",
-                        path: `/organization/profile`,
-                        param: `organizationId=${searchParams.get("organizationId")}`,
-                      },
-                      {
-                        title: t('common.settings'),
-                        label: "",
-                        icon: Settings,
-                        variant: "ghost",
-                        path: `/organization/settings`,
-                        param: `organizationId=${searchParams.get("organizationId")}`,
-                      },
-                    ] : [{
-                      title: t('common.profile'),
-                      label: "",
-                      icon: Gem,
-                      variant: "ghost",
-                      path: `/organization/profile`,
-                      param: `organizationId=${searchParams.get("organizationId")}`,
-                    }]}
+                    links={
+                      isAdmin
+                        ? [
+                            {
+                              title: t("common.profile"),
+                              label: "",
+                              icon: Gem,
+                              variant: "ghost",
+                              path: `/organization/profile`,
+                              param: `organizationId=${searchParams.get(
+                                "organizationId"
+                              )}`,
+                            },
+                            {
+                              title: t("common.settings"),
+                              label: "",
+                              icon: Settings,
+                              variant: "ghost",
+                              path: `/organization/settings`,
+                              param: `organizationId=${searchParams.get(
+                                "organizationId"
+                              )}`,
+                            },
+                          ]
+                        : [
+                            {
+                              title: t("common.profile"),
+                              label: "",
+                              icon: Gem,
+                              variant: "ghost",
+                              path: `/organization/profile`,
+                              param: `organizationId=${searchParams.get(
+                                "organizationId"
+                              )}`,
+                            },
+                          ]
+                    }
                   />
                 </ScrollArea>
               </div>
             </ResizablePanel>
-            <ResizableHandle withHandle />
-            <ResizablePanel className="h-[calc((100vh-68px))]" defaultSize={organizationLayout.layout[1]} minSize={30}>
+            <Separator
+              orientation="vertical"
+              className="h-[calc((100vh-68px))]"
+            />
+            {/* <ResizableHandle withHandle /> */}
+            <ResizablePanel
+              className="h-[calc((100vh-68px))]"
+              defaultSize={organizationLayout.layout[1]}
+              minSize={30}
+            >
               {children}
             </ResizablePanel>
           </ResizablePanelGroup>
-        </TooltipProvider>) : null
-    }</>
-  )
+        </TooltipProvider>
+      ) : null}
+    </>
+  );
 }
--- src/hooks/use-organizations.ts
+++ src/hooks/use-organizations.ts
@@ -1,38 +1,42 @@
-import { Database } from '@/types/database.types'
-import { atom, useAtomValue, useSetAtom } from 'jotai'
-import { atomWithStorage } from 'jotai/utils'
+import { Database } from "@/types/database.types";
+import { atom, useAtomValue, useSetAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
-const organizations = atom<Database['public']['Tables']['organizations']['Row'][]>([])
-const roles = atom<Database['public']['Views']['user_role_with_organizations']['Row'][]>([])
+const organizations = atom<
+  Database["public"]["Tables"]["organizations"]["Row"][]
+>([]);
+const roles = atom<
+  Database["public"]["Views"]["user_role_with_organizations"]["Row"][]
+>([]);
 
 const organizationLayout = atomWithStorage<{
-  layout: number[]
-  collapsed: boolean
-}>('organization-layout', {
+  layout: number[];
+  collapsed: boolean;
+}>("organization-layout", {
   layout: [265, 1095],
-  collapsed: false,
-})
+  collapsed: true,
+});
 
 export const useOrganizations = () => {
-  return useAtomValue(organizations)
-}
+  return useAtomValue(organizations);
+};
 
 export const useSetOrganizations = () => {
-  return useSetAtom(organizations)
-}
+  return useSetAtom(organizations);
+};
 
 export const useRoles = () => {
-  return useAtomValue(roles)
-}
+  return useAtomValue(roles);
+};
 
 export const useSetRoles = () => {
-  return useSetAtom(roles)
-}
+  return useSetAtom(roles);
+};
 
 export const useOrganizationLayout = () => {
-  return useAtomValue(organizationLayout)
-}
+  return useAtomValue(organizationLayout);
+};
 
 export const useSetOrganizationLayout = () => {
-  return useSetAtom(organizationLayout)
-}
+  return useSetAtom(organizationLayout);
+};
