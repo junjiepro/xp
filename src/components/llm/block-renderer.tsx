757b8bb3222513090f84ea5b43bbf1d599eb9489
Junjie <2721795866@qq.com>
2024-07-01 22:38:17 +0800

    llm model setting

--- src/components/llm/block-renderer.tsx
+++ src/components/llm/block-renderer.tsx
@@ -9,8 +9,10 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { Trash, PlusCircle, BrainCircuit, FileBox } from "lucide-react";
+import { Trash, PlusCircle, BrainCircuit, FileBox, Check } from "lucide-react";
 import { EdittingBlock, XpModel } from "@/types/datas.types";
+import { Label } from "../ui/label";
+import { Switch } from "../ui/switch";
 
 const URLBlockRenderer = (
   block: EdittingBlock<string[]> | undefined,
@@ -152,9 +154,50 @@ const ModelBlockRenderer = (
     };
     setBlock(next);
   };
+
+  const [value, setValue] = React.useState<string>("");
+  const [name, setName] = React.useState<string>("");
+  const nextValue = (
+    k: string,
+    key: keyof XpModel,
+    val: string | string[] | boolean
+  ) => {
+    const next = {
+      ...block,
+      id: block?.id || "",
+      block: {
+        ...block?.block,
+        [k]: {
+          base_url: block?.block?.[k]?.base_url || "",
+          model: block?.block?.[k]?.model || "",
+          tokenizer: block?.block?.[k]?.tokenizer || "",
+          config: block?.block?.[k]?.config || "",
+          quantized: block?.block?.[k]?.quantized || false,
+          seq_len: block?.block?.[k]?.seq_len || 2048,
+          size: block?.block?.[k]?.size || "",
+          [key]: val,
+        },
+      },
+      access: {
+        ...block?.access,
+        owners: [...(block?.access?.owners || [])],
+        roles: [...(block?.access?.roles || [])],
+      },
+    };
+    return next;
+  };
   return (
     <>
-      <Accordion type="single" collapsible className="w-full">
+      <Accordion
+        value={value}
+        onValueChange={(v) => {
+          setValue(v);
+          setName(v);
+        }}
+        type="single"
+        collapsible
+        className="w-full"
+      >
         {Object.entries(block?.block || {}).map(([k, v]) => (
           <AccordionItem key={k} value={k}>
             <AccordionTrigger>
@@ -163,8 +206,232 @@ const ModelBlockRenderer = (
                 <span>{k}</span>
               </div>
             </AccordionTrigger>
-            <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
+            <AccordionContent className="p-1 space-y-2">
+              <div className="flex flex-row items-center justify-between">
+                <div className="w-32">Name</div>
+                <div className="flex flex-row items-center gap-1">
+                  <Input
+                    value={name}
+                    autoFocus
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                  />
+
+                  <Button
+                    variant={"ghost"}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      const key = name || k;
+                      const next = {
+                        ...block,
+                        id: block?.id || "",
+                        block: {
+                          ...block?.block,
+                          [key]: {
+                            base_url: block?.block?.[k]?.base_url || "",
+                            model: block?.block?.[k]?.model || "",
+                            tokenizer: block?.block?.[k]?.tokenizer || "",
+                            config: block?.block?.[k]?.config || "",
+                            quantized: block?.block?.[k]?.quantized || false,
+                            seq_len: block?.block?.[k]?.seq_len || 2048,
+                            size: block?.block?.[k]?.size || "",
+                          },
+                        },
+                        access: {
+                          ...block?.access,
+                          owners: [...(block?.access?.owners || [])],
+                          roles: [...(block?.access?.roles || [])],
+                        },
+                      };
+                      if (key !== k) {
+                        delete next.block[k];
+                        setValue(key);
+                      }
+                      setBlock(next);
+                    }}
+                  >
+                    <Check className="w-4 h-4" />
+                  </Button>
+                  <Button
+                    variant={"ghost"}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      const next = {
+                        ...block,
+                        id: block?.id || "",
+                        block: {
+                          ...block?.block,
+                        },
+                        access: {
+                          ...block?.access,
+                          owners: [...(block?.access?.owners || [])],
+                          roles: [...(block?.access?.roles || [])],
+                        },
+                      };
+                      delete next.block[k];
+                      setBlock(next);
+                    }}
+                  >
+                    <Trash className="w-4 h-4 text-destructive" />
+                  </Button>
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="w-32">Base URL</div>
+                <Input
+                  value={v.base_url}
+                  onChange={(e) => {
+                    setBlock(nextValue(k, "base_url", e.target.value));
+                  }}
+                />
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="w-32">Model</div>
+                <div className="flex flex-col items-center justify-end gap-1">
+                  {Array.isArray(v.model) ? (
+                    v.model.map((i, index) => (
+                      <div
+                        key={index}
+                        className="flex flex-row items-center justify-end"
+                      >
+                        <Input
+                          value={i}
+                          onChange={(e) => {
+                            setBlock(
+                              nextValue(
+                                k,
+                                "model",
+                                Array.isArray(v.model)
+                                  ? v.model.map((v, i) =>
+                                      i === index ? e.target.value : v
+                                    )
+                                  : e.target.value
+                              )
+                            );
+                          }}
+                        />
+                        <Button
+                          variant={"ghost"}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setBlock(
+                              nextValue(
+                                k,
+                                "model",
+                                Array.isArray(v.model)
+                                  ? [...v.model, ""]
+                                  : [v.model, ""]
+                              )
+                            );
+                          }}
+                        >
+                          <PlusCircle className="w-4 h-4" />
+                        </Button>
+                        {v.model.length > 1 ? (
+                          <Button
+                            variant={"ghost"}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              setBlock(
+                                nextValue(
+                                  k,
+                                  "model",
+                                  Array.isArray(v.model)
+                                    ? v.model.filter((v, i) => i !== index)
+                                    : v.model
+                                )
+                              );
+                            }}
+                          >
+                            <Trash className="w-4 h-4 text-destructive" />
+                          </Button>
+                        ) : null}
+                      </div>
+                    ))
+                  ) : (
+                    <div className="flex flex-row items-center justify-end">
+                      <Input
+                        value={v.model}
+                        onChange={(e) => {
+                          setBlock(
+                            nextValue(
+                              k,
+                              "model",
+                              Array.isArray(v.model)
+                                ? [e.target.value]
+                                : e.target.value
+                            )
+                          );
+                        }}
+                      />
+                      <Button
+                        variant={"ghost"}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setBlock(
+                            nextValue(
+                              k,
+                              "model",
+                              Array.isArray(v.model)
+                                ? [...v.model, ""]
+                                : [v.model, ""]
+                            )
+                          );
+                        }}
+                      >
+                        <PlusCircle className="w-4 h-4" />
+                      </Button>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="w-32">Tokenizer</div>
+                <Input
+                  value={v.tokenizer}
+                  onChange={(e) => {
+                    setBlock(nextValue(k, "tokenizer", e.target.value));
+                  }}
+                />
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="w-32">Config</div>
+                <Input
+                  value={v.config}
+                  onChange={(e) => {
+                    setBlock(nextValue(k, "config", e.target.value));
+                  }}
+                />
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="w-32">Quantized</div>
+                <Switch
+                  checked={v.quantized}
+                  onCheckedChange={(e) =>
+                    setBlock(nextValue(k, "quantized", e))
+                  }
+                />
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="w-32">Seq len</div>
+                <Input
+                  value={v.seq_len}
+                  type="number"
+                  onChange={(e) => {
+                    setBlock(nextValue(k, "seq_len", e.target.value));
+                  }}
+                />
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="w-32">Size</div>
+                <Input
+                  value={v.size}
+                  onChange={(e) => {
+                    setBlock(nextValue(k, "size", e.target.value));
+                  }}
+                />
+              </div>
             </AccordionContent>
           </AccordionItem>
         ))}
