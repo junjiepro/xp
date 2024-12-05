53f279a3b64ef8c2417066a38c419e1874dd3753
Junjie <2721795866@qq.com>
2024-12-05 22:42:06 +0800

    ✨ feat: application
    add pglite application

--- src/components/applications/index.tsx
+++ src/components/applications/index.tsx
@@ -2,10 +2,12 @@
 
 import React from "react";
 import exampleRegister from "@/lib/applications/example";
+import pgliteRegister from "@/lib/applications/pglite";
 
 export default function ApplicationsRegister() {
     React.useEffect(() => {
         exampleRegister();
+        pgliteRegister();
     }, []);
     return null;
 }
