e1bf62749cd59d111c99d000b5ff832f1d305030
Junjie <2721795866@qq.com>
2025-04-02 00:23:55 +0800

    supabase provider

--- src/lib/applications/supabase/server.ts
+++ src/lib/applications/supabase/server.ts
@@ -1,13 +1,40 @@
 import { Database } from "@/types/database.types";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
+import UserProfileDao from "../supabase/dao/user-profile";
+import UserDeviceDao from "../supabase/dao/user-device";
+import OrganizationDao from "../supabase/dao/organization";
+import RoleDao from "../supabase/dao/role";
+import SettingBlockDao from "../supabase/dao/setting-block";
+import ApplicationBlockDao from "../supabase/dao/application-block";
+import { AuthChangeEvent, Session } from "@supabase/supabase-js";
+
 export const supabase = createClientComponentClient<Database>();
 
-/**
- * Signs out the user from the Supabase authentication service.
- *
- * @return {Promise<void>} A promise that resolves when the user is signed out successfully.
- */
-export const signOut = async () => {
-  return await supabase.auth.signOut();
-};
+class SupabaseProvider {
+  private supabase = supabase;
+
+  private userProfileDao = new UserProfileDao();
+  private userDeviceDao = new UserDeviceDao();
+  private organizationDao = new OrganizationDao();
+  private roleDao = new RoleDao();
+  private settingBlockDao = new SettingBlockDao();
+  private applicationBlockDao = new ApplicationBlockDao();
+
+  /// Auth
+
+  onAuthStateChange(
+    callback: (
+      event: AuthChangeEvent,
+      session: Session | null
+    ) => void | Promise<void>
+  ) {
+    return this.supabase.auth.onAuthStateChange(callback);
+  }
+
+  async signOut() {
+    return await this.supabase.auth.signOut();
+  }
+}
+
+export { SupabaseProvider };
