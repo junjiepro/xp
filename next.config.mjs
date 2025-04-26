329c8565decc86a961f10312607940c7fe4e097a
Junjie <2721795866@qq.com>
2025-04-26 21:17:53 +0800

    pglite in nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export', images: { unoptimized: true } };
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  transpilePackages: [
+    '@electric-sql/pglite',
+  ],
+};
 
 export default nextConfig;
